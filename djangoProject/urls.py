b931d18851b2729cde18f443e34045b8a33dd1df
astapov <dmytroastapow@gmail.com>
2024-01-09 20:05:34 +0200

    rename project

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for shop_vikki_toys project.
+URL configuration for djangoProject project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
